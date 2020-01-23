bfc0a74415f4d2fcb69b945b893117d9d73013bd
Steve Krantzman <skrantzman@gmail.com>
2020-01-23 14:17:16 -0800

    Updated Comment and ReadMe

--- Dynamic Tables with Subtotals.js
+++ Dynamic Tables with Subtotals.js
@@ -1,8 +1,13 @@
-  // Create Dynamic Tables with optional subtotals row.
+  // CREATE DYNAMIC TABLES WITH OPTIONAL SUBTOTALS ROW.
   function CreateDynamicTable(target, heading, tblid = "", subtotal, XFD, p1Name, p1, p2Name = "ddl2", p2 = 0) {
     
-    //Request JSON data
-    var URL = '{url to your json data here}'; // replace curly brackets with url to dynaic data feed
+    // REQUEST JSON DATA FROM FEED
+
+    // I uses the jQuery getJSON function to return JSON encoded data from a feed.
+    // Seeing my data is from the same origin, this is not an issue for me.
+    // You should use whatever method you need, in order to return your JSON data.
+
+    var URL = '{url to your JSON data here}'; // replace curly brackets with url to dynamic data feed
 
     $.getJSON(URL, {
         "xfd" : XFD,
@@ -11,11 +16,13 @@
         [p2Name] : p2
     }, 
 
-    // On Successfully Getting Data Create Table
+    // ON SUCCESSFULLY GETTING DATA, USE SUCCESS FUNCTION TO DYNAMICALLY CREATE HTML TABLE
+
     function(myData){
-        //Start with headings
-        // EXTRACT KEY, FROM KEY:VALUE PAIRS IN YOUR JSON OBJECT, FOR THE HTML HEADERS, AND PUSH TO AN ARRAY. 
-        // EXAMPLE ('ClinicID', 'Clinic', 'Department', 'Assignment', 'Column For Each Date')
+
+        // Start by determining what the table headings are going to be.
+        // Extract the Keys, From Key:Value Pairs in Your JSON Object, for The Html Headers, and Push to an Array. 
+        // Example ('Clinic', 'Department', 'Assignment', 'ColumnNameForEachDate')
         var col = [];
         for (var i = 0; i < myData.length; i++) {
             for (var key in myData[i]) {
@@ -25,37 +32,37 @@
             }
         }
 
-        // CREATE THE DYNAMIC TABLE OBJECT
+        // Create the table element then assign it an Id and Class
         var table = document.createElement("table");
         $(table).attr({
           id: tblid,
           class: 'table table-bordered table-striped'
         });
 
-        // CREATE/ADD THE TABLE HEAD SECTION
+        // Add the table head section element
         var thead = table.createTHead();
         
-        // CREATE/ADD THE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ARRAY ABOVE.
-        var tr = thead.insertRow(-1);                   // INSERT TABLE HEAD ROW.
+        // Add the html table header row using the extracted headers array above.
+        var tr = thead.insertRow(-1);                   // Insert a row element into the table header.
 
         for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // CREATE THE TH ELEMENT FOR EACH HEADER IN THE ARRAY.
+            var th = document.createElement("th");      // Add a th element for each header in the array.
             th.innerHTML = col[i];
             tr.appendChild(th);
         }
 
-        // CREATE/ADD THE TABLE BODY SECTION
+        // Add the table body element
         var tbody = table.createTBody();
 
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        // Insert a row for each record in the JSON Feed
+        // Add JSON data to the table body.
+        // Insert a row for each record in the JSON Feed as a tr element
         for (var i = 0; i < myData.length; i++) {       
             tr = tbody.insertRow(-1);
 
             for (var j = 0; j < col.length; j++) {
                 var tabCell = tr.insertCell(-1);
-                // Add the sum class to all columns after the 3rd column
-                // The sum class needs to be set on the columns you wat to subtotal in the footer
+                // Add the "sum" class to all columns after the 3rd column
+                // The sum class needs to be set on the columns you want to subtotal in the footer
                 if (j > 2) {
                     tabCell.setAttribute('class', 'sum');
                 }
@@ -64,18 +71,20 @@
             }
         }
 
-        // CREATE/ADD THE TABLE FOOTER ROW
-        // insert a final row for the subtotal row of the table and set the class to subhead if subtotal exists.
+        // CREATE/ADD THE TABLE FOOTER ROW (not actually a footer, but the last row in the table body).
+        // insert a subtotal row for the table and set the class to subhead if subtotal = true.
         if (subtotal === true) {
           tr = table.insertRow(-1);
           tr.setAttribute('class', 'subhead');
 
           for (var j = 0; j < col.length; j++) {
               var tabCell = tr.insertCell(-1);
+
               // Add the heading for the subtotal row.
               if (j == 0) {
                   tabCell.innerHTML="Total Daily Volunteer Counts";
               }
+
               // Add the subtotal class to all columns after the 3rd column
               if (j > 2) {
                   tabCell.setAttribute('class', 'subtotal');
@@ -113,13 +122,13 @@
         }     
     })
 
-    // On Error - return error - return no results found.
+    // ON ERROR - RETURN ERROR "No Results Found".
     .error(function() {
        Sys.WebForms.PageRequestManager.getInstance().add_endRequest(endRequestHandler) 
         function endRequestHandler(sender, args) {
-        document.getElementById(target).innerHTML = "<h4>" + heading + "</h4><h5 class='resultError'>No Results Found</h5>"
+        document.getElementById(target).innerHTML = "<h4>" + heading + "</h4><h5 class='resultError'>No Results Found For " + p1 + "</h5>"
         $('.' + p1Name).val(p1);
         $('.' + p2Name).val(p2);
        }   
     }); 
-  }  // End CreateDynamicTable Function!
+  }  // END CREATEDYNAMICTABLE FUNCTION!
